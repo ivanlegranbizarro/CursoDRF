921c7a614dd9af5b106b80cb7092faeffe575dfd
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-21 17:28:22 +0200

    modelos

--- product/models.py
+++ product/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from mptt.models import MPTTModel, TreeForeignKey
 
 # Create your models here.
+
+
+class Category(MPTTModel):
+    name = models.CharField(
+        "Name",
+        max_length=50,
+        validators=[MinLengthValidator(2), MaxLengthValidator(50)],
+        null=False,
+        blank=False,
+    )
+    parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(
+        "Name",
+        max_length=50,
+        validators=[MinLengthValidator(2), MaxLengthValidator(50)],
+        null=False,
+        blank=False,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(
+        "Name",
+        max_length=50,
+        validators=[MinLengthValidator(2), MaxLengthValidator(50)],
+        null=False,
+        blank=False,
+    )
+    description = models.TextField(
+        "Description",
+        max_length=500,
+        validators=[MinLengthValidator(10), MaxLengthValidator(500)],
+        null=True,
+        blank=True,
+        is_digital=models.BooleanField(default=False),
+    )
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = TreeForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
