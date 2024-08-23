de0d2492aa1f19ae3952c8e4d5a611762bae5138
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-23 09:06:53 +0200

    setup factoryboy

---
+++ ecommerceDRF/tests/product/test_models.py
@@ -0,0 +1,16 @@
+class TestCategporyModel:
+    def test_str_method(self):
+        # Arrange
+        # Act
+        # Assert
+        pass
+
+
+class TestBrandModel:
+    def test_str_method(self):
+        pass
+
+
+class TestProductModel:
+    def test_str_method(self):
+        pass
