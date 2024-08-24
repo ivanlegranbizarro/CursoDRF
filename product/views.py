724e1b245e3a32c0a69620c6b318919bbc89d14c
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-08-24 08:55:25 +0200

    list_product_by_category

--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Brand, Category, Product
@@ -42,4 +43,20 @@ class ProductViewSet(viewsets.ViewSet):
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    @extend_schema(responses=ProductSerializer)
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path=r"category/(?P<category>[\w-]+)/all",
+        url_name="list_product_by_category",
+    )
+    def list_product_by_category(self, request, category=None):
+        """
+        This is and enpoint for listing products by category
+        """
+        serializer = ProductSerializer(
+            self.queryset.filter(category__name=category), many=True
+        )
         return Response(serializer.data)
